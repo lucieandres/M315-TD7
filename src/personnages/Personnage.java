c1dd4abe9ef52781df03b4da48fc4fd97cf01d17
lucieandres <lucie.andres@etu.unice.fr>
2021-12-08 15:01:49 +0100

    Update Personnage.java

--- src/personnages/Personnage.java
+++ src/personnages/Personnage.java
@@ -3,4 +3,27 @@ package personnages;
 public abstract class Personnage {
 	private String nom;
 	private int vie;
+	
+	public Personnage(String nom) {
+		this.nom = nom;
+		this.vie = 100;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getVie() {
+		return vie;
+	}
+
+	public void setVie(int vie) {
+		this.vie = vie;
+	}
+	
+	
 }
