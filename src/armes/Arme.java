08f5bf1268f961c8307db8e151421c31d63ea254
Benjamin Neuville <b.neuville@outlook.fr>
2021-12-15 13:50:34 +0100

    se battre

--- src/armes/Arme.java
+++ src/armes/Arme.java
@@ -5,7 +5,23 @@ public abstract class Arme {
 	private int protection;
 	
 	public Arme(int f, int p) {
-		force = f;
-		protection = p;
+		setForce(f);
+		setProtection(p);
+	}
+
+	public int getProtection() {
+		return protection;
+	}
+
+	public void setProtection(int protection) {
+		this.protection = protection;
+	}
+
+	public int getForce() {
+		return force;
+	}
+
+	public void setForce(int force) {
+		this.force = force;
 	}
 }
--- src/personnages/Personnage.java
+++ src/personnages/Personnage.java
@@ -1,11 +1,14 @@
 package personnages;
 
+import java.util.ArrayList;
+
 import armes.Arme;
 
 public abstract class Personnage {
 	private String nom;
 	private int vie;
-	private Arme arme;
+	private Arme main;
+	private ArrayList<Arme> sac;
 	
 	public Personnage(String nom, Arme arme) {
 		this.nom = nom;
@@ -13,11 +16,19 @@ public abstract class Personnage {
 	}
 
 	public Arme getArme() {
-		return arme;
+		return main;
 	}
 
 	public void setArme(Arme arme) {
-		this.arme = arme;
+		this.main = arme;
+	}
+	
+	public ArrayList<Arme> getSac() {
+		return sac;
+	}
+
+	public void setSac(ArrayList<Arme> sac) {
+		this.sac = sac;
 	}
 
 	public String getNom() {
@@ -37,7 +48,9 @@ public abstract class Personnage {
 	}
 	
 	public void seBattre(Personnage adversaire) {
-		
+		int coef = this.getArme().getForce()-adversaire.getArme().getProtection();
+		if(coef>=0)
+			adversaire.setVie(adversaire.getVie()-coef);
 	}
 	
 }
