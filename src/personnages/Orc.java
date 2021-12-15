c98f226d838c949f227c0d3555ae4588babd0f6c
lucieandres <lucie.andres@etu.unice.fr>
2021-12-15 13:47:39 +0100

    Update Orc.java

--- src/personnages/Orc.java
+++ src/personnages/Orc.java
@@ -1,10 +1,13 @@
 package personnages;
 
+import armes.Epee;
+
 public class Orc extends Personnage{
 	int quotienFureur;
+	static Epee epee;
 
 	public Orc(String nom, int quotienFureur) {
-		super(nom);
+		super(nom, epee);
 		this.quotienFureur = quotienFureur;
 	}
 
